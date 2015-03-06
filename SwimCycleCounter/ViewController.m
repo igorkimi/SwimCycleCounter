f85abe8db729c73ee3f7134740a261ce4ede8909
Igor Kimieciki <igorkimieciki@gmail.com>
2015-03-06 15:32:45 -0300

    Project added to repo

--- SwimCycleCounter/ViewController.m
+++ SwimCycleCounter/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *chronometer;
+
+@property (weak, nonatomic) NSMutableArray *cycles;
+@property (weak, nonatomic) NSDate *startTime;
+@property (weak, nonatomic) NSTimer *mainTimer;
 
 @end
 
@@ -22,6 +27,55 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)startTime:(id)sender {
+    NSLog(@"Start");
+    
+    self.startTime = [NSDate date];
+    
+    NSLog(@"%@", self.startTime);
+    
+    self.mainTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/100 target:self selector:@selector(refreshTimer:) userInfo:nil repeats:YES];
+    
+}
+
+
+- (void)refreshTimer:(NSTimer*)timer {
+    
+    
+    NSLog(@"Refresh Timer");
+    
+    
+    
+    
+    //NSMutableString *text = [[NSMutableString alloc] init];
+    NSDateFormatter *formatter;
+    NSString        *dateString;
+    
+    formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"mm:ss.SSS"];
+    dateString = [formatter stringFromDate:[NSDate date]];
+    
+    NSLog(@"%@",dateString);
+    // decrement timer 1 … this is your UI, tick down and redraw
+    //[myStopwatch tickDown];
+    //[myStopwatch.view setNeedsDisplay];
+    
+    // increment timer 2 … bump time and redraw in UI
+    //…
+}
+
+- (void)actionStop:(id)sender {
+    
+    // stop the timer
+}
+
+- (IBAction)stopTimer:(id)sender {
+    NSLog(@"Stop");
+    [self.mainTimer invalidate];
+}
+- (IBAction)addCycle:(id)sender {
+    NSLog(@"Add Cycle");
 }
 
 @end
