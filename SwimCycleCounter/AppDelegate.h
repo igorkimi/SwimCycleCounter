71ada9d090ed1becb346acc94e9b2bc617883fb1
Igor Kimieciki <igor@MacBook-Pro-de-Igor.local>
2015-03-06 14:53:34 -0300

    Initial Commit

---
+++ SwimCycleCounter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwimCycleCounter
+//
+//  Created by Igor Kimieciki on 06/03/15.
+//  Copyright (c) 2015 Igor Kimieciki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwimCycleCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwimCycleCounter
+//
+//  Created by Igor Kimieciki on 06/03/15.
+//  Copyright (c) 2015 Igor Kimieciki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
